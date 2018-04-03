0f6d0103c598d1e51e8f8fe4ff7c84163531ba3a
as66422 <aditya.ranglal.singhania@citi.com>
2018-04-03 19:40:51 +0530

    initial commit

---
+++ ace-kafka.py
@@ -0,0 +1,1 @@
+from kafka import KafkaConsumer, KafkaProducerimport sysconsumer = KafkaConsumer(sys.argv[1], bootstrap_servers='acetlegt1d:9092')producer = KafkaProducer(bootstrap_servers='acetlegt1d:9092')for msg in consumer:    print(msg)    s = str(msg).encode('UTF-8')    producer.send('KAFKA_RESPONSE_TOPIC', key=msg.topic.encode('UTF-8'), value=msg.value)
